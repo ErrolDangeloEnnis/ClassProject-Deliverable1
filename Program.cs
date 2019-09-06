b5536ccb6b354058b3ac34b44511e6015292b749
Errol Ennis <erroldangelo@usf.edu>
2019-09-05 21:06:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Name: Errol Ennis
+// Hello World assignment
+// Due: 09/05/2019
+
+namespace ClassProject_Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pause to display hello world!
+            Console.WriteLine("Press any Key to Continue...");
+            Console.WriteLine("Hello World! This is the first deliverable for this course completed by Errol Ennis.");
+            Console.ReadKey(true);
+        }
+    }
+}
